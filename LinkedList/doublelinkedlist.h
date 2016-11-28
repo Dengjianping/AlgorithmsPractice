11c28130fa1b397b867c158b0d2c155b6fbb52b7
Dengjianping <djptux@gmail.com>
2016-11-28 17:43:46 +0800

    update

--- LinkedList/doublelinkedlist.h
+++ LinkedList/doublelinkedlist.h
@@ -5,10 +5,39 @@ template <class T>
 struct DoubleLinkedList
 {
     T value;
-    DoubleLinkedList* pre, next;
+    DoubleLinkedList* prev, next;
     DoubleLinkedList(T v)
     {
         value = n;
-        pre = next = NULL;
+        prev = next = NULL;
     }
 };
+
+template <class T>
+void createDoubleLinkedList(const int countOfNodes, DoubleLinkedList<T>* head)
+{
+    DoubleLinkedList<T>* x = head;
+    for (size_t i = 0; i < countOfNodes; i++)
+    {
+        DoubleLinkedList<T>* t = new DoubleLinkedList<T>(randNumber());
+        x->next = t;
+        t->prev = x;
+        x = t;
+    }
+}
+
+template <class T>
+void deleteNode(DoubleLinkedList<T>* specified)
+{
+    specified->prev->next = specified->next;
+    specified->next->prev = specified->prev;
+}
+
+template <class T>
+void addNode(DoubleLinkedList<T>* location, DoubleLinkedList<T>* newNode)
+{
+    DoubleLinkedList<T>* temp = location->next;
+    location->next = newNode;
+    newNode->next = temp;
+    temp->prev = newNode;
+}
